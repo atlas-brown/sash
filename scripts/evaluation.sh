d071524907d4c08aaaf47e542062d0310eb34d94
George Kapetanakis <george.kapetanakis.gk@gmail.com>
2025-10-14 14:44:33 -0400

    Compare output with ground truth in evaluation

--- scripts/evaluation.sh
+++ scripts/evaluation.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 # Run all benchmarks named posix.sh in the benchmarks directory and subdirectories
 # The benchmarks directory is located by recursively searching outwards from the current directory
@@ -12,15 +12,29 @@ if [ -n "$1" ]; then
     bench_dir="$bench_dir/$1"
 fi
 
+failure=0
+
 # Find all files or symlinks named posix.sh in the benchmarks directory and subdirectories
-find "$bench_dir" -type f -name 'posix.sh' -o -type l -name 'posix.sh' | while read -r benchmark; do
+while read -r benchmark; do
     echo "Running benchmark: $benchmark"
     output=$(uv run sash "$benchmark")
 
-    # Check if the output is valid JSON
-    if ! echo "$output" | jq empty; then
-        echo "$output"
-        continue
+    # Input: { "errors": [ { "code": ... }, ... ] }
+    # Output: [ { "code": ... }, ... ]
+    expected=$(jq --sort-keys '.errors' "$(dirname "$benchmark")/ground_truth.json")
+
+    # Input: { ..., "errors": [ { "code": ..., message: ... }, ... ] }
+    # Output: [ { "code": ... }, ... ]
+    actual=$(echo "$output" | jq --sort-keys '[.errors[] | {code}]' 2>/dev/null)
+
+    if [ "$expected" != "$actual" ]; then
+        echo "Unexpected output:"
+        echo "$output" | jq '.errors'
+        failure=$((failure + 1))
     fi
-    echo "$output" | jq -r '.error_messages[]' || true
-done
+done < <(find "$bench_dir" -type f -name 'posix.sh' -o -type l -name 'posix.sh')
+
+if [ "$failure" -ne 0 ]; then
+    echo "$failure benchmarks failed"
+    exit 1
+fi
