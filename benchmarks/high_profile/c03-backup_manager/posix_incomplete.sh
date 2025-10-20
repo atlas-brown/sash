d0430036a6dbb24158036ae28d276f9a9bb71d17
George Kapetanakis <george.kapetanakis.gk@gmail.com>
2025-10-20 15:41:31 -0400

    Add IDs to high profile benchmarks Fix high profile ground truths

--- benchmarks/high_profile/c03-backup_manager/posix_incomplete.sh
+++ benchmarks/high_profile/c03-backup_manager/posix_incomplete.sh
@@ -200,7 +200,7 @@ __exec_meta_command()
                 $command 1> $file_to_create 2>$logfile
             fi
 
-            if [ $? -gt 0 ]; then
+            if [ $? -gt 0 ]; then # bug here: intent was to check the exit status of the first command in the pipeline on line 197; right now even if it fails the log is deleted
                 warning "Unable to exec \$command; check \$logfile"
                 rm -f $file_to_create
             else
