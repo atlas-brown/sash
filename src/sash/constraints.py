a5c2e0ca6e38e8b5521d71c151c91195d90ef885
Evangelos Lamprou <vagos@lamprou.xyz>
2025-11-05 21:58:49 -0500

    Add constraints
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- src/sash/constraints.py
+++ src/sash/constraints.py
@@ -1,5 +1,63 @@
+from __future__ import annotations  # for postponed evaluation of annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from sash.state import SymStr
+
+from dataclasses import dataclass
 
 class Constraint:
     pass
 
-# todo bring over stuff from shseer PRUs
+class Empty(Constraint):
+    pass
+
+@dataclass(frozen=True)
+class Not(Constraint):
+    constraint: Constraint
+
+@dataclass(frozen=True)
+class And(Constraint):
+    lhs: Constraint
+    rhs: Constraint
+
+@dataclass(frozen=True)
+class Or(Constraint):
+    lhs: Constraint
+    rhs: Constraint
+
+@dataclass(frozen=True)
+class StringEq(Constraint):
+    lhs: SymStr
+    rhs: SymStr
+
+@dataclass(frozen=True)
+class IsFile(Constraint):
+    path: SymStr
+
+@dataclass(frozen=True)
+class IsDir(Constraint):
+    path: SymStr
+
+@dataclass(frozen=True)
+class IsDeleted(Constraint):
+    path: SymStr
+
+@dataclass(frozen=True)
+class ReadsPath(Constraint):
+    path: SymStr
+
+@dataclass(frozen=True)
+class WritesPath(Constraint):
+    path: SymStr
+
+@dataclass(frozen=True)
+class CommandExists(Constraint):
+    name: SymStr
+
+@dataclass(frozen=True)
+class HasStdout(Constraint):
+    command: SymStr
+
+@dataclass(frozen=True)
+class ExpectsStdin(Constraint):
+    command: SymStr
