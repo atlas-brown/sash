ea62d993576eb33df76e356fd01344ad48a43b45
Evangelos Lamprou <vagos@lamprou.xyz>
2025-11-07 15:05:18 -0500

    Add utils
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- src/sash/util.py
+++ src/sash/util.py
@@ -1,4 +1,4 @@
-
+from sash.state import *
 from typing import Any
 
 
@@ -23,3 +23,9 @@ def split_at(l: list[Any], element: Any) -> list[list[Any]]:
             current.append(item)
     result.append(current)
     return result
+
+def constant_field(string: str, words: int = 1) -> Field:
+    return Field(SymStr((string,)), WordCount(words, words))
+
+def shasta_pretty(ast_node: Any) -> str:
+    return ast_node.pretty() if hasattr(ast_node, 'pretty') else str(ast_node)
