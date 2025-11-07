0dbb02455e212b8922a40a73d50cc0b86d5c41c4
Evangelos Lamprou <vagos@lamprou.xyz>
2025-11-06 21:37:42 -0500

    Reorganize assertion_to_z3
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- src/sash/solver.py
+++ src/sash/solver.py
@@ -22,6 +22,7 @@ def field_to_z3(field_content: SymStr | CompletelyArbitrary) -> z3.ExprRef:
             return z3_var
 
 def constraint_to_z3(constraint: Constraint, s: State):
+    logging.debug(f"Translating constraint to Z3: {constraint}")
     match constraint:
         case Empty() | HasStdout() | ExpectsStdin() | Reads() | Writes():
             return z3.BoolVal(True)
@@ -53,9 +54,12 @@ def state_to_z3(s: State) -> z3.ExprRef:
         env_formula.append(eq_formula)
     env_formula = z3.And(env_formula)
 
-    return z3.And(s.fs_model.state_to_z3(), pathcond_formula, env_formula)
+    fs_state_formula = s.fs_model.state_to_z3()
 
-def assertion_to_z3(solver: z3.Solver, assertion: Assertion) -> tuple[z3.BoolRef, z3.ExprRef]:
+    return z3.And(fs_state_formula, pathcond_formula, env_formula)
+
+def assertion_to_z3(assertion: Assertion) -> tuple[z3.BoolRef, z3.ExprRef]:
+    logging.debug(f"Translating assertion to Z3: {assertion}")
     assertion_var = z3.FreshBool('assertion')
     constraint_formula = constraint_to_z3(assertion.constraint, assertion.producing_state)
     state_formula = state_to_z3(assertion.producing_state)
@@ -91,11 +95,11 @@ def run_solver(traces: list[Trace], config: InterpConfig) -> list[Report]:
     solver.set(unsat_core=True)
 
     for trace in traces:
+        logging.debug(f"Processing trace with {(trace.latest_state.assertions)} assertions for Z3 solver.")
         assertions = trace.latest_state.assertions
         for assertion in assertions:
-            assertion_var, assertion_formula = assertion_to_z3(solver, assertion)
+            assertion_var, assertion_formula = assertion_to_z3(assertion)
             solver.assert_and_track(assertion_formula, assertion_var)
-
 
     result = solver.check()
     if result == z3.unsat:
