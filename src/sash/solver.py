386fcf9470dcaca45846c97f93622c9b39e56506
Evangelos Lamprou <vagos@lamprou.xyz>
2025-11-06 23:26:04 -0500

    Fix arbitrary handling
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- src/sash/solver.py
+++ src/sash/solver.py
@@ -14,12 +14,13 @@ def field_to_z3(field_content: SymStr | CompletelyArbitrary) -> z3.ExprRef:
         case CompletelyArbitrary() as arbitrary:
             arbitrary_no_pfx_sfx = replace(arbitrary, prefix=None, suffix=None)
             if arbitrary_no_pfx_sfx not in arbitrary_to_z3_var:
-                arbitrary_to_z3_var[arbitrary_no_pfx_sfx] = z3.FreshConst(z3.StringSort(), arbitrary.source.pretty())
+                arbitrary_to_z3_var[arbitrary_no_pfx_sfx] = z3.FreshConst(z3.StringSort(), "arbitrary")
             z3_var = arbitrary_to_z3_var[arbitrary_no_pfx_sfx]
             z3_var = z3.Concat(field_to_z3(arbitrary.prefix) if arbitrary.prefix else z3.StringVal(""),
                                z3_var,
                                field_to_z3(arbitrary.suffix) if arbitrary.suffix else z3.StringVal(""))
             return z3_var
+    assert False, f"Expected field content, got {field_content}"
 
 def constraint_to_z3(constraint: Constraint, s: State):
     match constraint:
@@ -49,7 +50,7 @@ def state_to_z3(s: State) -> z3.ExprRef:
     env_formula = []
     for var, val in s.env.items():
         var_z3 = z3.String(var)
-        val_z3 = field_to_z3(val.value)
+        val_z3 = field_to_z3(val.value.content)
         eq_formula = (var_z3 == val_z3)
         env_formula.append(eq_formula)
     env_formula = z3.And(env_formula)
@@ -59,8 +60,8 @@ def state_to_z3(s: State) -> z3.ExprRef:
     return z3.And(fs_state_formula, pathcond_formula, env_formula)
 
 def assertion_to_z3(assertion: Assertion) -> tuple[z3.BoolRef, z3.ExprRef]:
-    logging.debug(f"Translating assertion to Z3: {assertion}")
     assertion_var = z3.FreshBool('assertion')
+    tracked_assertions[assertion_var] = assertion
     constraint_formula = constraint_to_z3(assertion.constraint, assertion.producing_state)
     state_formula = state_to_z3(assertion.producing_state)
 
