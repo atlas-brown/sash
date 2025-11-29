422360d26aeaa44eeda3617bf05978b48ad2ad13
Evangelos Lamprou <vagos@lamprou.xyz>
2025-11-29 01:35:58 -0500

    Remove duplicate check
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- src/sash/symb.py
+++ src/sash/symb.py
@@ -692,17 +692,10 @@ def expand_simple(stuff: list[AST.ArgChar],
                         _, expanded_args = expand_args_dumb(t, b.node.arguments, config)
                         if expanded_args:
                             cmd_name = expanded_args[0].try_to_str()
-                            if isinstance(cmd_name, str):
-                                # If the command is `mkdir` and the verbose flag is not present, the output is empty, which implies a command substitution capturing empty output.
-                                if cmd_name == "mkdir":
-                                    literal_args = [f.try_to_str() for f in expanded_args[1:] if f.try_to_str() is not None]
-                                    if "-v" not in literal_args:
-                                        Reporter.add_issue(reporter.CapturingEmptyOutput(cmd_name, context_line))
-                                # Otherwise, check the spec to see if the command has no stdout. If it does, report the same issue.
-                                else:
-                                    if spec := get_spec(cmd_name, tuple(expanded_args)):
-                                        if spec.io in {IOType.NONE, IOType.STDIN}:
-                                            Reporter.add_issue(reporter.CapturingEmptyOutput(cmd_name, context_line))
+                            if isinstance(cmd_name, str) and (spec := get_spec(cmd_name, tuple(expanded_args))):
+                                # Check the spec to see if the command has no stdout. If it does, report an issue.
+                                if spec.io in {IOType.NONE, IOType.STDIN}:
+                                    Reporter.add_issue(reporter.CapturingEmptyOutput(cmd_name, context_line))
                     self.add_a_field(arbitrary_field(b, ArbitraryType.APPROXIMATION, self.state))
                 case _:
                     logging.error("argchar: %s %s", argchar.pretty(), type(argchar))
