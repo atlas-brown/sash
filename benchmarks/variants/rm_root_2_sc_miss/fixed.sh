47c30ca877efa444360baff430712cddf5360771
Lukas Lazarek <LukasALazarek@gmail.com>
2026-02-05 09:32:58 -0500

    Add rm_root_2_sc_miss variant

---
+++ benchmarks/variants/rm_root_2_sc_miss/fixed.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+gather_data()
+{
+  # Gather data
+  df -lP && df -lP > df-lP.out
+  df -lh && df -lh > df-lh.out
+  [ -f /proc/swaps ] && cat /proc/swaps > proc_swaps.out
+  [ -f /proc/cpuinfo ] && cat /proc/cpuinfo > cpuinfo.out
+  which free && free -m > free-m.out
+  which prtdiag && prtdiag > prtdiag.out
+  [ -f /etc/redhat-release ] && cp /etc/redhat-release .
+  [ -f /etc/issue ] && cp /etc/issue .
+  which swap && swap -l > swap-l.out
+  # etc etc
+}
+
+TEMPDIR=$(mktemp -d)
+cd "${TEMPDIR}"
+gather_data
+tar cf /tmp/logs.tar "${TEMPDIR}"
+gzip -9 /tmp/logs.tar
+cd /
+rm -rf "${TEMPDIR:?}"/
