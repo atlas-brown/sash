8bd8aed461ca96b17f9251c80f9e195d51c19c0e
Evangelos Lamprou <vagos@lamprou.xyz>
2026-02-05 13:51:52 -0500

    Add const_loop variant
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

---
+++ benchmarks/variants/const_loop_sc_fp/posix.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+touch while/151234
+touch while/152355
+touch while/151694
+touch while/153699
+touch while/156946
+NUMSNAPS=$(ls while | awk '{print $1}' | wc -l)
+RETAIN=2
+
+while [ "$RETAIN" -le "$NUMSNAPS" ]; do # bug here: NUMSNAPS is constant (infinite loop)
+    OLDEST=$(ls | awk '{print $1}' | head -n 1)
+    rm "$OLDEST"
+done
