19cd8ee57406d6b9a160ff679587af242b2b54eb
Evangelos Lamprou <vagos@lamprou.xyz>
2026-08-22 21:51:58 +0300

    Add shellcheck eval script
    Signed-off-by: Evangelos Lamprou <vagos@lamprou.xyz>

--- benchmarks/bugs_and_variants/m2-rm_root/variants/bug-too_many_diffs.sh
+++ benchmarks/bugs_and_variants/m2-rm_root/variants/bug-too_many_diffs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 COMMENT="//" # diff
 DUMP=mongodump
-OUT_DIR=/data/backup/mongod/tmp   "$COMMENT" a # bug here: '//' is interpreted as a command
+OUT_DIR=/data/backup/mongod/tmp/  "$COMMENT" a # bug here: '//' is interpreted as a command
 TAR_DIR=/data/backup/mongod       "$COMMENT" b # bug here: '//' is interpreted as a command
 DATE=`date +%Y_%m_%d_%H_%M_%S`    "$COMMENT" c # bug here: '//' is interpreted as a command
 DB_USER=Guitang                   "$COMMENT" d # bug here: '//' is interpreted as a command
@@ -9,7 +9,7 @@ DB_PASS=qq                        "$COMMENT" e # bug here: '//' is interpreted a
 DAYS=14                           "$COMMENT" f # bug here: '//' is interpreted as a command
 TAR_BAK="mongod_bak_$DATE.tar.gz" "$COMMENT" g # bug here: '//' is interpreted as a command
 cd $OUT_DIR                       "$COMMENT" h # bug here: unbound variable, too many arguments
-rm -rf  $OUT_DIR/*                "$COMMENT" i # bug here: unbound variable, will try to delete root
+rm -rf  "$OUT_DIR"*               "$COMMENT" i # bug here: unbound variable, will try to delete root
 mkdir -p $OUT_DIR/$DATE           "$COMMENT" j # bug here: unbound variable
 $DUMP -d wecard -u $DB_USER -p $DB_PASS -o $OUT_DIR/$DATE   "$COMMENT" k # ...
 tar -zcvf $TAR_DIR/$TAR_BAK $OUT_DIR/$DATE       "$COMMENT" l # ...
